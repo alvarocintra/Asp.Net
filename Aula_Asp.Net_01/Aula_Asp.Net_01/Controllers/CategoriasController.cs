c7b2cf2841bbc15ae7fcd94f559cfb564a9cab2e
Álvaro Cintra <alvarocintra21@gmail.com>
2018-05-15 22:01:08 -0300

    Atualizando

--- Aula_Asp.Net_01/Aula_Asp.Net_01/Controllers/CategoriasController.cs
+++ Aula_Asp.Net_01/Aula_Asp.Net_01/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula_Asp.Net_01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,11 +12,16 @@ namespace Aula_Asp.Net_01.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
-            categorias.Add("Estudo");
-            categorias.Add("Lazer");
-            categorias.Add("Refeição");
-            categorias.Add("Trabalho");
+            List<Categoria> categorias = new List<Categoria>();
+            categorias.Add(
+                new Categoria()
+                {
+                    Nome = "Estudo"
+                }
+                );
+            categorias.Add(new Categoria() { Nome = "Casa" });
+            categorias.Add(new Categoria() { Nome = "Carro" });
+            categorias.Add(new Categoria() { Nome = "Trabalho" });
 
             ViewBag.Categorias = categorias;
 
